191ed51a52a227c43035257160893c46b21cf8bd
Peter Bjorklund <piot@hotmail.com>
2023-04-24 23:05:50 +0200

    chore: clean up cmake file

--- src/lib/posix/clog.c
+++ src/lib/posix/clog.c
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 #include <clog/clog.h>
 
 char g_clog_temp_str[CLOG_TEMP_STR_SIZE];
